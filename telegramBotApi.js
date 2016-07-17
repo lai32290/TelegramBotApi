fe4c83588e59ada38361278b48408fb3fc7a7bc6
Lai <lai32690@gmail.com>
2016-07-17 12:03:22 -0300

    add parse mode constant

--- telegramBotApi.js
+++ telegramBotApi.js
@@ -6,7 +6,7 @@ const request = require('request')
     ;
 
 const config = {
-    host: 'api.telegram.org'
+    host: 'api.bot.org'
     , baseUrl : 'https://api.telegram.org/bot'
     , methods: {
         getUpdates: 'getUpdates'
@@ -14,70 +14,85 @@ const config = {
     }
 };
 
-function TelegraBotApi(token) {
+const parseMode = {
+    markdown: 'Markdown'
+    , html: 'HTML'
+};
+
+function getUpdates() {
     var self = this;
-    self.token = token !== undefined ? token : undefined;
 
-    self.getUpdates = getUpdates;
-    self.sendMessage = sendMessage;
+    return new Promise((resolve, reject) => {
+        const options = {
+            url : _makeUrl(self.token, config.methods.getUpdates)
+            , json : true
+        };
 
-    function getUpdates() {
-        return new Promise((resolve, reject) => {
-            const options = {
-                url : _makeUrl(config.methods.getUpdates)
-                , json : true
-            };
+        request.get(options, (err, res, body) => {
+            if(err) reject(err);
 
-            request.get(options, (err, res, body) => {
-                if(err) reject(err);
-
-                resolve(body);
-            });
+            resolve(body);
         });
-    }
-
-    function sendMessage() {
-        var parametters = {};
-
-        switch (arguments.length) {
-            case 1:
-                parametters = arguments[0];
-                break;
-
-            case 2:
-                parametters = {
-                    'chat_id' : arguments[0]
-                    , 'text' : arguments[1]
-                };
-                break;
-
-            default:
-                parametters = extend({
-                    'chat_id': arguments[0]
-                    , 'text': arguments[1]
-                }, arguments[3]);
-                break;
-        }
-
-        parametters = qs.stringify(parametters);
-
-        return new Promise((resolve, reject) => {
-            const options = {
-                url: _makeUrl(config.methods.sendMessage) + '?' + parametters
-                , json: true
-            };
-
-            request.get(options, (err, res, body) => {
-                if(err) reject(err);
-
-                resolve(body);
-            });
-        });
-    }
-
-    function _makeUrl(method) {
-        return config.baseUrl + self.token + '/' + method;
-    }
+    });
 }
 
-module.exports = TelegraBotApi;
+function sendMessage() {
+    var self = this;
+
+    var parametters = {};
+
+    switch (arguments.length) {
+        case 1:
+            parametters = arguments[0];
+            break;
+
+        case 2:
+            parametters = {
+                'chat_id' : arguments[0]
+                , 'text' : arguments[1]
+            };
+            break;
+
+        default:
+            parametters = extend({
+                'chat_id': arguments[0]
+                , 'text': arguments[1]
+            }, arguments[2]);
+            break;
+    }
+
+    console.log(parametters);
+
+    parametters = qs.stringify(parametters);
+
+    return new Promise((resolve, reject) => {
+        const options = {
+            url: _makeUrl(self.token, config.methods.sendMessage) + '?' + parametters
+            , json: true
+        };
+
+        request.get(options, (err, res, body) => {
+            if(err) reject(err);
+
+            resolve(body);
+        });
+    });
+}
+
+function _makeUrl(token, method) {
+    return config.baseUrl + token + '/' + method;
+}
+
+function TelegraBotApi(token) {
+    var self = this;
+
+    if(token) self.token = token;
+}
+
+TelegraBotApi.prototype.getUpdates = getUpdates;
+TelegraBotApi.prototype.sendMessage = sendMessage;
+
+module.exports = {
+    bot: TelegraBotApi
+    , parseMode: parseMode
+};
