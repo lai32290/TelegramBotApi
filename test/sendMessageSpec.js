ee4a1d7bfaecb015c4939a6bf0467953db3a6433
Lai <lai32690@gmail.com>
2016-07-23 07:06:32 -0300

    add replyKeyboard test in sendMessage

--- test/sendMessageSpec.js
+++ test/sendMessageSpec.js
@@ -12,28 +12,43 @@ const chatId = config.chatId;
 
 describe('sendMessage', function() {
     this.timeout(10000);
+    const options = {
+        'chat_id': chatId,
+        'text': 'Send message'
+    };
 
     it('only require data', function() {
-        const testName = 'only require data';
-        return bot.sendMessage(chatId, testName)
+        return bot.sendMessage(options.chat_id, options.text)
             .then(function(res) {
                 assert.property(res, 'ok');
                 assert.equal(res.ok, true);
-                assert.equal(res.result.text, testName);
+                assert.equal(res.result.text, options.text);
             });
     });
 
     it('only require data with json', function() {
-        const testName = 'only require data with json';
-        const options = {
-            'chat_id': chatId,
-            'text': testName
-        };
         return bot.sendMessage(options)
             .then(function(res) {
                 assert.property(res, 'ok');
                 assert.equal(res.ok, true);
-                assert.equal(res.result.text, testName);
+                assert.equal(res.result.text, options.text);
+            });
+    });
+
+    it('send reply keyboard', function() {
+        var op = JSON.parse(JSON.stringify(options));
+        const keyboard = {
+            keyboard: [
+                [{text: 'option 1'}, {text: 'option 2'}]
+                , [{text: 'option 3'}, {text: 'option 4'}]
+            ]
+            , one_time_keyboard: true
+        };
+        op.reply_markup = JSON.stringify(keyboard);
+        return bot.sendMessage(op)
+            .then(function(res) {
+                assert.property(res, 'ok');
+                assert.equal(res.ok, true);
             });
     });
 });
